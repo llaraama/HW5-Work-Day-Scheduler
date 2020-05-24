6bc8b5dd9e52d7c4a20a046388f28e769f53dfe7
Laura Lara <llaraama@uncc.edu>
2020-05-23 20:32:43 -0400

    HW5

---
+++ script.js
@@ -0,0 +1,110 @@
+/* current time on title  */
+var currentDate=moment().format('LLLL');
+$("#currentDay").append(currentDate);
+
+let weekTimes=[
+    "9","10","11","12","13","14", "15", "16","17"
+];
+
+let weekATimes=[
+    "9am","10am","11am","12pm","1pm","2pm", "3pm", "4pm","5pm",
+];
+
+
+
+
+var time= moment().hours();
+console.log(time)
+weekTimes.forEach(function(timesText){
+
+    
+    /* time block outter */
+    var blockTime=$("<div>");
+    $(".container").append(blockTime);
+    blockTime.attr("class","time-block");
+    blockTime.attr("data-time",timesText);
+
+    /* row goes inside time block outter */
+    var rowInput=$("<div>");
+    $(".time-block").append(rowInput);
+    rowInput.attr("class","row");
+    
+
+
+
+    /* hour goes inside row */
+    var hourInsert=$("<div>");
+    hourInsert.text(timesText);
+    hourInsert.attr("class","hour");
+   
+
+    /* description goes inside row */
+    var textArea=$("<textarea>");
+    textArea.attr("class","description");
+
+    /* saveBtn goes inside row */
+    var buttonSave=$("<div>");
+    buttonSave.text("save button");
+    buttonSave.attr("class","saveBtn");
+    
+
+    /* adding it to hour, description and saveBtn to row */
+    rowInput.append(hourInsert,textArea,buttonSave);
+
+    
+
+    // $(document).ready(function(){
+    //     $('.saveBtn').click(function(event){
+    //         event.preventDefault();
+    //         localStorage.setItem(".description",JSON.stringify())
+    //     });
+    // });
+    
+    
+
+
+
+    // var saveInput= document.querySelector('.saveBtn');
+
+    // var saveInsert=document.querySelector('.description');
+
+    // saveInput.addEventListener("click", function(event){
+
+    // event.preventDefault();
+
+    // var userInput={
+    //     UserInput:saveInsert.value.trim()
+    // };
+
+    // localStorage.setItem("userInput",JSON.stringify(userInput));
+
+    // var lastInput=JSON.parse(localStorage.getItem("userInput"));
+
+
+
+
+    // saveInsert.textContent=lastInput.UserInput;
+
+    // });
+
+
+   
+    
+if (timesText<time){
+    textArea.attr("class","past description");
+    
+}
+else if(timesText==time){
+    textArea.attr("class","present description");
+}
+else{
+    textArea.attr("class","future description");
+
+}
+
+console.log(timesText)
+});
+
+
+
+
